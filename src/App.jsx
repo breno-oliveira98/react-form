802ae3a45b3e51e14671e2c12f2d9a2ee02b0500
Breno Oliveira <135337339+breno-oliveira98@users.noreply.github.com>
2024-10-18 19:18:41 +0000

    exibindo resultado em Json ao digitar e reset do form criado

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,19 @@ import { InputSelect } from "./components/input/InputSelect";
 function App() {
   const [formData, setFormData] = useState({})
   const handleChange = (e) => {
-    setFormData({...formData, [e.target.id]: e.target.value })
+    setFormData({...formData, [e.target.id]: e.target.value }
+    )    
+    console.log(JSON.stringify(formData));
+    
   }
-  const [nomeCompleto, setNomeCompleto] = useState('');
+
+  const handleReset = () => {
+    setFormData({})
+  }
+
+  const resultado = JSON.stringify(formData, null, 2)
+
+
   const enviar = (e) => {
     e.preventDefault();
     console.log('Form', formData)
@@ -18,7 +28,7 @@ function App() {
     <>
       <main className="form-pessoa ">
         <h2>Cadstro de Pessoa</h2>
-        <form className="row g-3" onSubmit={enviar}>
+        <form className="row g-3" onSubmit={enviar} onReset={handleReset}>
         <Input
          type="text"
          id={'nome'}
@@ -29,64 +39,75 @@ function App() {
          type="text"
          id={'nome-mae'}
          label={'Nome Mãe'}
+         handleChange={handleChange}
          />
         <Input
          type="date"
          id={'data-nascimento'}
          inputSize={'2'}
          label={'Data de Nascimento'}
+         handleChange={handleChange}
          />
         <Input
          type="email"
          id={'email'}
          inputSize={'5'}
          label={'Email'}
+         handleChange={handleChange}
          />
         <Input
          type="password"
          id={'senha'}
          inputSize={'5'}
          label={'Senha'}
+         handleChange={handleChange}
          />
         <Input
          type="text"
          id={'cep'}
          inputSize={'3'}
          label={'CEP'}
+         handleChange={handleChange}
          />
         <Input
          type="text"
          id={'endereco'}
          inputSize={'9'}
          label={'Endereço'}
+         handleChange={handleChange}
          />
         <Input
          type="text"
          id={'numero'}
          inputSize={'1'}
          label={'Número'}
+         handleChange={handleChange}
          />
         <Input
          type="text"
          id={'complemento'}
          inputSize={'11'}
          label={'Complemento'}
+         handleChange={handleChange}
          />
         <Input
          type="text"
          id={'bairro'}
          inputSize={'4'}
          label={'Bairro'}
+         handleChange={handleChange}
          />
          <InputSelect
          id={'estado'}
          inputSize={'4'}
          label={'Estado'}
+         handleChange={handleChange}
          />
          <InputSelect
          id={'cidade'}
          inputSize={'4'}
          label={'Cidade'}
+         handleChange={handleChange}
          />
          <Button
          name={'Cadastrar'}
@@ -99,8 +120,9 @@ function App() {
          type={'reset'}
          />
         </form>
-        <div>
-          {nomeCompleto}
+        <div className="text-white">
+        <pre>{resultado !== '{}' ? resultado : ''}
+        </pre>
         </div>
       </main>
     </>
